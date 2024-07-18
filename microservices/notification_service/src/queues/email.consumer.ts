d04c85c3dcb10f90e45fe5205bbcc5b762f1d9d0
KyawKyawThar <kyawkyaw.thar84@gmail.com>
2024-07-19 00:33:58 +0630

    implement consume auth email and order email message using ElasticSearch

--- microservices/notification_service/src/queues/email.consumer.ts
+++ microservices/notification_service/src/queues/email.consumer.ts
@@ -7,25 +7,51 @@ import { createConnection } from '@notifications/queues/connection';
 
 const logger: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'notificationEmailConsumer', 'debug');
 
-async function consumeAuthEmailMessage(channel: Channel): Promise<void> {
+export async function consumeAuthEmailMessage(channel: Channel): Promise<void> {
   try {
     if (!channel) {
       const createChannel = await createConnection();
       if (createChannel) channel = createChannel;
     }
 
-    const exchangeName = 'email-notification';
-    const queueName = 'auth-queue-name';
-    const routingKey = 'auth-email-key';
+    await channel.assertExchange(config.EMAIL_EXCHANGE_NAME, 'direct');
+    const assertQueue = await channel.assertQueue(config.EMAIL_QUEUE_NAME, { durable: true, autoDelete: false });
+    await channel.bindQueue(assertQueue.queue, config.EMAIL_EXCHANGE_NAME, config.EMAIL_ROUTING_KEY);
 
-    await channel.assertExchange(exchangeName, 'direct');
-    const assertQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
-    await channel.bindQueue(assertQueue.queue, exchangeName, routingKey);
-
-    channel.consume(assertQueue.queue, async (msg: ConsumeMessage | null) => {
+    await channel.consume(assertQueue.queue, async (msg: ConsumeMessage | null) => {
+      //consume the message
       console.log(JSON.parse(msg!.content.toString()));
+      if (msg) {
+        //const message = JSON.parse(msg!.content.toString());
+
+        channel.ack(msg);
+      }
     });
   } catch (err) {
     logger.log('error', 'NotificationService EmailConsumer consumeAuthEmailMessages() method error', err);
+  }
+}
+
+export async function consumeOrderEmailMessage(channel: Channel): Promise<void> {
+  try {
+    if (!channel) {
+      const createChannel = await createConnection();
+
+      if (createChannel) channel = createChannel;
+    }
+    await channel.assertExchange(config.ORDER_EXCHANGE_NAME, 'direct');
+
+    const assertQueue = await channel.assertQueue(config.ORDER_QUEUE_NAME, { durable: true, autoDelete: false });
+    await channel.bindQueue(assertQueue.queue, config.ORDER_EXCHANGE_NAME, config.ORDER_ROUTING_KEY);
+
+    await channel.consume(assertQueue.queue, async (msg: ConsumeMessage | null) => {
+      console.log(JSON.parse(msg!.content.toString()));
+
+      if (msg) {
+        channel.ack(msg);
+      }
+    });
+  } catch (err) {
+    logger.log('error', 'NotificationService OrderEmailConsumer consumeOrderEmailMessage() method error', err);
   }
 }
